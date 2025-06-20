59b4e9ef8897a6cacbd2a25d46b717cba1afd3de
elias <eliasfaculdade@yahoo.com.br>
2025-06-20 17:35:47 -0300

    iniciando testes unitarios

---
+++ DapperDemoApp/DapperDemoApp.Tests/Services/ClienteServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using DapperDemoApp.Models;
+using DapperDemoApp.Services;
+using NUnit.Framework;
+
+namespace DapperDemoApp.Tests
+{
+    [TestFixture]
+    public class ClienteServiceTests
+    {
+        private ClienteService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new ClienteService();
+        }
+
+        [Test]
+        public void Adicionar_DeveAdicionarCliente()
+        {
+            var cliente = new Cliente { Nome = "Elias", Email = "elias@email.com" };
+
+            _service.Adicionar(cliente);
+            var resultado = _service.ObterTodos();
+
+            //Assert.AreEqual(1, resultado.Count());
+            //Assert.AreEqual("Elias", resultado.First().Nome);
+        }
+
+        [Test]
+        public void Atualizar_DeveAlterarDadosDoCliente()
+        {
+            var cliente = new Cliente { Nome = "Antigo", Email = "a@a.com" };
+            _service.Adicionar(cliente);
+
+            var atualizado = new Cliente { Nome = "Novo", Email = "novo@email.com" };
+            var sucesso = _service.Atualizar(1, atualizado);
+
+            //Assert.IsTrue(sucesso);
+            //Assert.AreEqual("Novo", _service.ObterPorId(1).Nome);
+        }
+
+        [Test]
+        public void Remover_DeveExcluirCliente()
+        {
+            _service.Adicionar(new Cliente { Nome = "Remover", Email = "x@x.com" });
+
+            var sucesso = _service.Remover(1);
+            //Assert.IsTrue(sucesso);
+            //Assert.IsNull(_service.ObterPorId(1));
+        }
+    }
+}
+
---
+++ DapperDemoApp/DapperDemoApp.Tests/Services/ProdutoServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using DapperDemoApp.Services;
+using NUnit.Framework;
+
+namespace DapperDemoApp.Tests
+{
+    [TestFixture]
+    public class ProdutoServiceTests
+    {
+        [Test]
+        public void InserirProdutosDeApiMock_DeveExecutarSemErro()
+        {
+            var service = new ProdutoService();
+
+            Assert.DoesNotThrow(() => service.InserirProdutosDeApiMock());
+        }
+    }
+}
